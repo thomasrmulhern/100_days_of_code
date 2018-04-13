8a6896ceabbe5375eabb35cc0daffdefe438dd68
Thomas Mulhern <mulhern.thomasr@gmail.com>
2018-04-12 22:41:15 -0700

    Added first draft of convert_to_datetime function

--- datetime_practice.py
+++ datetime_practice.py
@@ -18,18 +18,32 @@ with open(logfile) as f:
 
 def convert_to_datetime(line):
     '''TODO 1:
-       Given a log line extract its timestamp and convert it to a datetime object. 
+       Given a log line extract its timestamp and convert it to a datetime object.
        For example calling the function with:
        INFO 2014-07-03T23:27:51 supybot Shutdown complete.
        returns:
        datetime(2014, 7, 3, 23, 27, 51)'''
-    pass
+    import re
+    line = 'INFO 2014-07-03T23:27:51 supybot Shutdown complete.'
+    x = re.search('(\d+\W){2}\d+', line)
+    y = re.search('(\d+\W){3}', line)
+    x = x.group(0)
+    y = y.group(0)
+    y = y.strip()
+    xy = list(x+y)
+    year = int(''.join(xy[0:4]))
+    month = int(''.join(xy[5:7]))
+    day = int(''.join(xy[8:10]))
+    hour = int(''.join(xy[10:12]))
+    second = int(''.join(xy[13:15]))
+    milisecond = int(''.join(xy[16:18]))
+
+    return datetime(year, month, day, hour, second, milisecond)
 
 
 def time_between_shutdowns(loglines):
     '''TODO 2:
-       Extract shutdown events ("Shutdown initiated") from loglines and calculate the 
-       timedelta between the first and last one. 
+       Extract shutdown events ("Shutdown initiated") from loglines and calculate the
+       timedelta between the first and last one.
        Return this datetime.timedelta object.'''
     pass
-
