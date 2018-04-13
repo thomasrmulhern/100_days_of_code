fa208dbef2d541b3024cc9fcaa5d433d8f0b44aa
Thomas Mulhern <mulhern.thomasr@gmail.com>
2018-04-13 08:29:18 -0700

    created another function inside convert_to_datetime to clean up datetime vars

--- datetime_practice.py
+++ datetime_practice.py
@@ -26,18 +26,34 @@ def convert_to_datetime(line):
        returns:
        datetime(2014, 7, 3, 23, 27, 51)'''
 
-    x = re.search('(\d+\W){2}\d+', line)
-    y = re.search('(\d+\W){3}', line)
-    x = x.group(0)
-    y = y.group(0)
-    y = y.strip()
-    xy = list(x+y)
-    year = int(''.join(xy[0:4]))
-    month = int(''.join(xy[5:7]))
-    day = int(''.join(xy[8:10]))
-    hour = int(''.join(xy[10:12]))
-    second = int(''.join(xy[13:15]))
-    milisecond = int(''.join(xy[16:18]))
+
+    x = list(re.search('(\d+\W){2}\d+', line).group(0))
+    y = list(re.search('(\d+\W){3}', line).group(0).strip())
+    xy = x+y
+
+    def form(beg, end):
+        return  int(''.join(xy[beg:end]))
+
+    year = form(0,4)
+    month = form(5,7)
+    day = form(8,10)
+    hour = form(10,12)
+    second = form(13,15)
+    milisecond = form(16,18)
+
+    # '''trying to convert the earlier code into a dictionary,
+    # then loop over it for the final return'''
+    #
+    # # should the dict values be lists of all strings, mixed types or just ints?
+
+    # time_dict = {
+    #     year : [0,4]
+    #     month : [5,7]
+    #     day : [8,10]
+    #     hour : [10,12]
+    #     second = [13,15]
+    #     milisecond = [16,18]
+    # }
 
     return datetime(year, month, day, hour, second, milisecond)
 
@@ -51,12 +67,13 @@ def time_between_shutdowns(loglines):
     line = [re.search("Shutdown initiated", loglines) for log in loglines]
     dt = [convert_to_datetime(lines) for lines in line]
     return datetime.timedelta(dt[len(dt)]-dt[0])
-    
 
 
-# line1 = 'ERROR 2014-07-03T23:24:31 supybot Invalid user dictionary file'
-# line2 = 'INFO 2015-10-03T10:12:51 supybot Shutdown initiated.'
-# line3 = 'INFO 2016-09-03T02:11:22 supybot Shutdown complete.'
-# print(convert_to_datetime(line1))
-# print(convert_to_datetime(line2))
-# print(convert_to_datetime(line3))
+# TESTS
+
+line1 = 'ERROR 2014-07-03T23:24:31 supybot Invalid user dictionary file'
+line2 = 'INFO 2015-10-03T10:12:51 supybot Shutdown initiated.'
+line3 = 'INFO 2016-09-03T02:11:22 supybot Shutdown complete.'
+print(convert_to_datetime(line1))
+print(convert_to_datetime(line2))
+print(convert_to_datetime(line3))
